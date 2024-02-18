d80643d33632849de0f73efc56f2cc004c2713d8
rustamnew <1rustamnew1@gmail.com>
2024-02-19 01:26:15 +0300

    Реализация

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script setup>
+    import AppHeader from './components/AppHeader.vue'
+    import AppFooter from './components/AppFooter.vue'
+    import Subscribe from './components/Subscribe.vue'
+    
 </script>
 
 <template>
+    <div class="max-w-5xl md:mx-auto">
+        <AppHeader />
+
+        <Subscribe />
+
+        <AppFooter />
+    </div>
 </template>
